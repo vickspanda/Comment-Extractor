743dee3e561422f8c3cc7f0f5d7893cf08486596
vickspanda <116336028+vickspanda@users.noreply.github.com>
2024-09-26 00:35:50 +0530

    final commit

---
+++ sample codes/test.py
@@ -0,0 +1,22 @@
+# This is a single-line comment explaining the main part of the code
+
+num = 10  # Inline comment explaining the variable
+
+"""
+This is a multi-line comment or docstring.
+It explains the following code block.
+"""
+if num > 5:
+    print("""#Number is greater than 5""")
+
+def add(a, b):
+    """
+    This is a docstring (multi-line comment) explaining the function.
+    This function adds two numbers and returns the sum.
+    
+    :param a: First number
+    :param b: Second number
+    :return: Sum of a and b
+    """
+    return a + b
+
